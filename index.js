0fd543dee7332143532b8b2a3574b7041c27561d
Tonni <hby3387@gmail.com>
2017-04-24 10:35:11 +0800

    - Add Env settings. - Fix code style issue.

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ module.exports = {
     extends: [
         'eslint-config-airbnb-base',
     ].map(require.resolve),
+    env: {
+        browser: true,
+        node: true,
+        jquery: true,
+    },
     rules: {
-        "indent": ["error", 4],
-        "max-len": ["error", {
-            "code": 120,
-            "ignoreComments": true
+        indent: ['error', 4],
+        'max-len': ['error', {
+            code: 120,
+            ignoreComments: true,
         }],
 
-        "require-jsdoc": ["error", {
-            "require": {
-                "FunctionDeclaration": true,
-                "MethodDefinition": true,
-                "ClassDeclaration": true,
-                "ArrowFunctionExpression": true
-            }
-        }]
-    }
+        'require-jsdoc': ['error', {
+            require: {
+                FunctionDeclaration: true,
+                MethodDefinition: true,
+                ClassDeclaration: true,
+                ArrowFunctionExpression: true,
+            },
+        }],
+    },
 };
