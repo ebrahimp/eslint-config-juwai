e17137a5106e54757afe025d9a92b573f8ff247a
Tonni <hby3387@gmail.com>
2017-08-01 18:19:15 +0800

    Bootstrap Project
    - Initialize config files.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = {
     extends: [
         'eslint-config-airbnb-base',
@@ -9,9 +11,31 @@ module.exports = {
     },
     rules: {
         indent: ['error', 4],
+        quotes: ['error', 'single'],
+        'eol-last': [0],
+        'no-mixed-requires': [0],
+        'no-underscore-dangle': [0],
         'max-len': ['error', {
             code: 120,
             ignoreComments: true,
+        }],
+
+        // Avoid using comma-dangle on function.
+        // https://github.com/eslint/eslint/issues/7851#issuecomment-270428874
+        'comma-dangle': ['error', {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'never',
+        }],
+
+        'no-alert': 'off',
+        'no-console': 'off',
+
+        // https://github.com/airbnb/javascript/issues/1071
+        'no-mixed-operators': ['error', {
+            allowSamePrecedence: true,
         }],
 
         'require-jsdoc': ['error', {
