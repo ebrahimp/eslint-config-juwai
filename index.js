c4f056220df54436a4ae840df90d93fe39eab129
Tonni <hby3387@gmail.com>
2017-08-04 17:09:45 +0800

    CON-1655
    - Add PR template file.
    - Remove unnecessary items in index.js.

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ module.exports = {
     extends: [
         'eslint-config-airbnb-base',
     ].map(require.resolve),
-    env: {
-        browser: true,
-        node: true,
-        jquery: true,
-    },
     rules: {
         indent: ['error', 4],
         quotes: ['error', 'single'],
